ef79a7bf24216c64d06b1023342f14cfd8a88d88
Frederic Jasmin <fredericjasmindixon@gmail.com>
2026-06-13 16:26:54 -0600

    juego
    añadi un mini juego en el index

--- js/script.js
+++ js/script.js
@@ -4,4 +4,60 @@ const nav = document.querySelector("nav");
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     nav.classList.toggle("show");
+});const objects = [
+    "Audifonos",
+    "Mochila",
+    "Laptop",
+    "Zapatos",
+    "Patineta",
+    "Lampara",
+    "Reloj",
+    "Botella",
+    "Libros",
+    "Taza"
+];
+
+let currentObject;
+
+const targetName =
+    document.querySelector("#target-name");
+
+const message =
+    document.querySelector("#game-message");
+
+function newGame() {
+
+    currentObject =
+        objects[Math.floor(Math.random() * objects.length)];
+
+    targetName.textContent = currentObject;
+
+    message.textContent = "";
+}
+
+document.querySelectorAll(".hitbox")
+.forEach(hitbox => {
+
+    hitbox.addEventListener("click", () => {
+
+        const clicked =
+            hitbox.dataset.object;
+
+        if (clicked === currentObject) {
+
+            message.textContent =
+                "✓ ¡Correcto!";
+
+            setTimeout(newGame, 1200);
+
+        } else {
+
+            message.textContent =
+                "✗ Sigue buscando";
+        }
+
+    });
+
 });
+
+newGame();
